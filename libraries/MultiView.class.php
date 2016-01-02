123e253996211d365eefc73e350b43e8df007097
wangxian <xian366@126.com>
2016-01-02 22:24:39 +0800

    更新：fork ePHP-v5, 减小部署体积，ePHP重新上路
    ePHP老版本地址：https://github.com/wangxian/ePHP-v4

---
+++ libraries/MultiView.class.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ +------------------------------------------------------------------------------
+ * 视图类
+ * 扩展view 使其支持多视图
+ +------------------------------------------------------------------------------
+ * @version 4.0
+ * @author WangXian
+ * @package libraries
+ * @email wo#wangxian.me
+ * @creation_date 2011-1-10
+ * @last_modified 2011-9-19
+ +------------------------------------------------------------------------------
+ */
+
+class MultiView extends view
+{
+	/**
+	 * 引用视图 或 视图片段
+	 * @param string $file 变量名
+	 * @param string $layout_block 是否渲染布局模版
+	 * @param boolean $layout_block 使用布局模版否
+	 * @param boolean $return 返回模版内容 or 直接输出
+	 */
+	public function _include($file, $__vars=null, $layout_block=false, $return=false)
+	{
+		if( is_array($this->vars) ) extract($this->vars);
+		if( is_array($__vars) ) extract($__vars);
+
+		/* support MultiView */
+		$file = $this->__filename($file);
+		$tpl = C('tpl_switch');
+
+		if(! file_exists(APP_PATH.'/views/'. $tpl .'/'.$file) )
+		{
+			if($tpl !== 'default')
+			{
+				 if(file_exists(APP_PATH.'/views/default/'.$file)) $tpl = 'default';
+				 else throw new ephpException("视图{$tpl}/{$file}不存在，公共视图default/{$file}也不存在,请保证其一存在!");
+			}
+			else throw new ephpException("主题视图{$tpl}/{$file}不存在。");;
+		}
+
+		$file=$tpl.'/'.$file;
+		/* support MultiView */
+
+		if($layout_block)
+		{
+			$tpl = C('tpl_switch');
+
+			ob_start();
+			include APP_PATH . '/views/'. $file;
+			$content = ob_get_clean();
+			//echo $content;print_r($this->_layout);exit;
+
+			if($this->_layout)
+			{
+				foreach ($this->_layout as $k=>$v) $content = str_replace ("<!--{layout_block_{$k}}-->", $v, $content);
+			}
+
+			//是否返回
+			if($return) return $content; else echo $content;
+		}
+		else if($return)
+		{
+			ob_start();include APP_PATH . '/views/'. $file;
+			return ob_get_clean();
+		}
+		else include APP_PATH . '/views/'. $file;
+	}
+}
---
+++ tpl/404.tpl.php
@@ -0,0 +1,18 @@
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8" />
+<title></title>
+<style type="text/css">
+body { font: 12px/18px Tahoma, Geneva, sans-serif; background-color: #f2f2f2; }
+#wrapper { margin: 50px auto; width: 500px; }
+h1 {font-size: 6em; color: #262626; line-height:1em; margin: 10px 0; border-bottom: 3px solid #CCC; }
+p { color: #666; letter-spacing: 1px;}
+</style>
+</head>
+<body>
+  <div id="wrapper">
+    <h1>404 ERROR... NOT!</h1>
+    <p>非常抱歉，没有找到您要访问的页面！</p>
+  </div>
+</body>
+</html>
